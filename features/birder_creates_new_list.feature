a027534936344965bddd91110c5989ecdb575c5e
GB Hoyt <qrprat77@gmail.com>
2014-09-03 11:21:32 -0400

    Modernized avian_knowledge.rb and started some features.

---
+++ features/birder_creates_new_list.feature
@@ -0,0 +1,6 @@
+Feature: Birder Creates a new List
+
+As a birder
+I want to create a new EBird List
+So I can record what I've seen
+
--- lib/ebird/avian_knowledge.rb
+++ lib/ebird/avian_knowledge.rb
@@ -4,22 +4,17 @@ require 'csv'
 module EBird
   class AvianKnowledge
     include HTTParty
-
-    base_uri 'www.avianknowledge.net/ws1.0'
+    base_uri 'ebird.org/ws1.1'
 
     def country_list
-      options = {:query => {:format => 'csv'}}
-      data = self.class.get("/ref/country/list", options)
-      data.gsub!(/,\n/, ',')
-      CSV.parse(data)
+      options = {:query => {:rtype => 'country', :format => 'csv'}}
+      self.class.get('/ref/location/list', options).parsed_response
     end
 
-    def subnational1_list(query_options={})
-      options = {:query => {:format => 'csv'}}
+    def subnational1_list(query_options={:countryCode => 'US'})
+      options = {:query => {:rtype => 'subnational1', :format => 'csv'}}
       options[:query].merge!(query_options)
-      data = self.class.get("/ref/subnational1/list", options)
-      data.gsub!(/,\n/, ',')
-      CSV.parse(data)
-    end
+      self.class.get("/ref/location/list", options).parsed_response
+     end
   end
 end
