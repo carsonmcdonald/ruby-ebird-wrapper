755f8160ef13596f6e2969e2164ced6d62e52ae7
GB Hoyt <qrprat77@gmail.com>
2014-09-21 03:12:53 -0400

    Making more feature, created branch

---
+++ features/step_definitions/sharing_lists_steps.rb
@@ -0,0 +1,11 @@
+Given(/^a birder already has an existing list$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^others went birding with them$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the birder can share the list with other birders who have an ebird account\.$/) do
+  pending # express the regexp above with the code you wish you had
+end
